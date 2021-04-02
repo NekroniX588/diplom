2db9e7dd3e2240236db8b3f18456fd7ae00e1ba0
Evgeniy Finogeev <finogeev@ap-team.ru>
2021-04-02 19:01:30 +0300

    calculate embeddings

--- Signature/identification/views.py
+++ Signature/identification/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+import os
+import numpy as np
+from .inference import get_embedding
 
 from .models import Signature
 from .forms import UploadForm
@@ -6,7 +8,6 @@ from .forms import UploadForm
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-
 
 class NewSignatureCreate(CreateView): # новый
 	model = Signature
@@ -16,6 +17,14 @@ class NewSignatureCreate(CreateView): # новый
 	def form_valid(self, form):
 		self.object = form.save(commit=False)
 		self.object.save()
+		embed = get_embedding(self.object.attach.path)
+		if os.path.exists('./checkpoints/embeddings.npy'):
+			embeddings = np.load('./checkpoints/embeddings.npy')
+			embeddings = np.append(embeddings, embed, axis=0)
+		else:
+			embeddings = embed
+		print('embeddings shape:', embeddings.shape)
+		np.save('./checkpoints/embeddings', embeddings)
 		return super().form_valid(form)
 
 def main(request):
